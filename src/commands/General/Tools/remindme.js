a8c4fb09cbe97cca851726615854595635abb4f2
Alexis M <iamsnkh@pm.me>
2019-02-23 19:08:17 +0100

    :hammer: Code refactoring and change description of remindme

--- src/commands/General/Tools/remindme.js
+++ src/commands/General/Tools/remindme.js
@@ -5,7 +5,7 @@ module.exports = class extends Command {
 
 	constructor(...args) {
 		super(...args, {
-			description: 'creates a reminder',
+			description: 'Créer un rappel',
 			usage: '<when:time> <text:...str>',
 			usageDelim: ', '
 		});
@@ -21,15 +21,15 @@ module.exports = class extends Command {
 		});
 
 		return msg.sendEmbed(new MessageEmbed()
-        .setAuthor("Reminder", "https://media.discordapp.net/attachments/548662983169933322/548690507249877013/latest.png")
-		.setThumbnail("https://media.giphy.com/media/hI2zGZkPL6boA/giphy.gif")
-		.setColor(0x00AE86)
-        .addField('❯ Utilisateur', msg.author, true)
-		.addField('❯ ID', reminder.id, true)
-		.addField('❯ Rappel', text, true)
-		.addBlankField(true)
-		.addField('❯ Expiration', when, true)
-        .setFooter(` ${msg.author.username}, l'horloge te surveille, désormais.. `, 'https://cdn-images-1.medium.com/max/800/0*3IFEy-hfoIpgFjBl.gif')
+			.setAuthor("Reminder", "https://media.discordapp.net/attachments/548662983169933322/548690507249877013/latest.png")
+			.setThumbnail("https://media.giphy.com/media/hI2zGZkPL6boA/giphy.gif")
+			.setColor(0x00AE86)
+			.addField('❯ Utilisateur', msg.author, true)
+			.addField('❯ ID', reminder.id, true)
+			.addField('❯ Rappel', text, true)
+			.addBlankField(true)
+			.addField('❯ Expiration', when, true)
+			.setFooter(` ${msg.author.username}, l'horloge te surveille, désormais.. `, 'https://cdn-images-1.medium.com/max/800/0*3IFEy-hfoIpgFjBl.gif')
 		);
 	}
 
--- src/tasks/reminder.js
+++ src/tasks/reminder.js
@@ -7,14 +7,14 @@ module.exports = class extends Task {
 		const _channel = this.client.channels.get(channel);
 
 		if (_channel) await _channel.sendEmbed(new MessageEmbed()
-        .setAuthor("Reminder", "https://media.discordapp.net/attachments/548662983169933322/548690507249877013/latest.png")
-		.setThumbnail("https://media.giphy.com/media/hI2zGZkPL6boA/giphy.gif")
-		.setColor(0x00AE86)
-		.addField('❯ Utilisateur', `<@${user}>`, true)
-		.addBlankField(true)
-		.addField('❯ Rappel', text, true)
-		.addBlankField(true)
-		.setFooter(`L'horloge ne te surveille plus, désormais.. `, 'https://cdn-images-1.medium.com/max/800/0*3IFEy-hfoIpgFjBl.gif')
+			.setAuthor("Reminder", "https://media.discordapp.net/attachments/548662983169933322/548690507249877013/latest.png")
+			.setThumbnail("https://media.giphy.com/media/hI2zGZkPL6boA/giphy.gif")
+			.setColor(0x00AE86)
+			.addField('❯ Utilisateur', `<@${user}>`, true)
+			.addBlankField(true)
+			.addField('❯ Rappel', text, true)
+			.addBlankField(true)
+			.setFooter(`L'horloge ne te surveille plus, désormais.. `, 'https://cdn-images-1.medium.com/max/800/0*3IFEy-hfoIpgFjBl.gif')
 		);
 	}
 
